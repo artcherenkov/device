dd35f05fa5ee71e4192d9293ebcc55ab85e7eb60
Артем Черенков <art.cherenkov@gmail.com>
2020-10-29 13:09:16 +0700

    Добавляет верстку попапа, оживляет его

---
+++ js/popup.js
@@ -0,0 +1,33 @@
+const openPopupBtn = document.querySelector(`.page-main__contact-more`);
+const popup = document.querySelector(`.popup`);
+const popupForm = popup.querySelector(`.popup form`);
+const closePopupBtn = popup.querySelector(`.popup__close-btn`);
+
+const inputs = popup.querySelectorAll(`.popup__input-wrapper input`);
+console.log(inputs);
+
+const OPEN_POPUP_CLASS = `popup--opened`;
+
+const handleOpenPopupBtnClick = (evt) => {
+  evt.preventDefault();
+  popupForm.reset();
+  popup.classList.add(OPEN_POPUP_CLASS);
+  closePopupBtn.addEventListener(`click`, handleClosePopupBtnClick);
+}
+
+const handleClosePopupBtnClick = () => {
+  popup.classList.remove(OPEN_POPUP_CLASS);
+}
+
+const handleEscKeydown = (evt) => {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains(OPEN_POPUP_CLASS)) {
+      evt.preventDefault();
+      popup.classList.remove(OPEN_POPUP_CLASS);
+      window.removeEventListener(`keydown`, handleEscKeydown);
+    }
+  }
+}
+
+openPopupBtn.addEventListener(`click`, handleOpenPopupBtnClick);
+window.addEventListener("keydown", handleEscKeydown);
