a5d595cd6facf9b9887cb04e6a3d6db4db9f847e
Артем Черенков <art.cherenkov@gmail.com>
2020-10-30 12:54:32 +0700

    Рефакторит модуль попапа

--- js/popup.js
+++ js/popup.js
@@ -1,32 +1,104 @@
-const openPopupBtn = document.querySelector(`.page-main__contact-more`);
-const popup = document.querySelector(`.popup`);
-const popupForm = popup.querySelector(`.popup form`);
-const closePopupBtn = popup.querySelector(`.popup__close-btn`);
+(function () {
+  const OPEN_POPUP_CLASS = `popup--opened`;
+  const INPUR_ERROR_CLASS = `popup__input--error`;
 
-const inputs = popup.querySelectorAll(`.popup__input-wrapper input`);
+  const openPopupBtn = document.querySelector(`.page-main__contact-more`);
+  const popup = document.querySelector(`.popup`);
+  const popupForm = popup.querySelector(`.popup form`);
+  const closePopupBtn = popup.querySelector(`.popup__close-btn`);
 
-const OPEN_POPUP_CLASS = `popup--opened`;
+  const popupInputs = popupForm.querySelectorAll(`.popup__input`);
 
-const handleOpenPopupBtnClick = (evt) => {
-  evt.preventDefault();
-  popupForm.reset();
-  popup.classList.add(OPEN_POPUP_CLASS);
-  closePopupBtn.addEventListener(`click`, handleClosePopupBtnClick);
-}
+  let errors = [];
 
-const handleClosePopupBtnClick = () => {
-  popup.classList.remove(OPEN_POPUP_CLASS);
-}
+  const createErrorMessage = () => {
+    const span = document.createElement(`span`);
+    span.textContent = `Тут пусто :(`;
+    span.classList.add(`popup__error-message`);
 
-const handleEscKeydown = (evt) => {
-  if (evt.keyCode === 27) {
-    if (popup.classList.contains(OPEN_POPUP_CLASS)) {
-      evt.preventDefault();
-      popup.classList.remove(OPEN_POPUP_CLASS);
-      window.removeEventListener(`keydown`, handleEscKeydown);
+    return span;
+  }
+
+  const clearErrors = ({input = null, fullRemove = false}) => {
+    if (errors) {
+      errors = errors.filter((error) => {
+        if (fullRemove ? true : error.input === input) {
+          resetDomError(error.error, error.input);
+        }
+        return fullRemove ? false : error.input !== input;
+      });
     }
   }
-}
 
-openPopupBtn.addEventListener(`click`, handleOpenPopupBtnClick);
-window.addEventListener("keydown", handleEscKeydown);
+  const resetDomError = (error, input) => {
+    error.remove();
+    input.classList.remove(INPUR_ERROR_CLASS);
+  }
+
+  const setError = (input) => {
+    const error = createErrorMessage();
+    errors.push({
+      input,
+      error
+    });
+    input.parentNode.appendChild(error);
+    input.classList.add(INPUR_ERROR_CLASS);
+  }
+
+  const handleOpenPopupBtnClick = (evt) => {
+    evt.preventDefault();
+    popupForm.reset();
+    popup.classList.add(OPEN_POPUP_CLASS);
+    closePopupBtn.addEventListener(`click`, handleClosePopupBtnClick);
+  }
+
+  const handleClosePopupBtnClick = () => {
+    popup.classList.remove(OPEN_POPUP_CLASS);
+    popupForm.reset();
+    clearErrors({fullRemove: true});
+  }
+
+  const handleEscKeydown = (evt) => {
+    if (evt.key === 27) {
+      if (popup.classList.contains(OPEN_POPUP_CLASS)) {
+        evt.preventDefault();
+        popup.classList.remove(OPEN_POPUP_CLASS);
+        window.removeEventListener(`keydown`, handleEscKeydown);
+      }
+    }
+  }
+
+  const handleFormSubmit = (evt) => {
+    if (checkFormValidity()) {
+      evt.preventDefault();
+    }
+  }
+
+  const checkFormValidity = () => {
+    clearErrors({fullRemove: true});
+    for (let input of popupInputs) {
+      if (!input.value) {
+        setError(input);
+      }
+    }
+    return Boolean(errors.length);
+  }
+
+  const handleInput = (input) => {
+    return () => {
+      clearErrors({input});
+      input.classList.remove(INPUR_ERROR_CLASS);
+      if (!input.value) {
+        setError(input);
+      }
+    }
+  }
+
+  popupForm.addEventListener(`submit`, handleFormSubmit);
+  for (let input of popupInputs) {
+    input.addEventListener(`input`, handleInput(input));
+  }
+  openPopupBtn.addEventListener(`click`, handleOpenPopupBtnClick);
+  window.addEventListener("keydown", handleEscKeydown);
+
+})();
