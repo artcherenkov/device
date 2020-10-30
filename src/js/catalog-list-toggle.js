1882e6c4b343e9e97bdff4b7249152a2b86f6512
Артем Черенков <art.cherenkov@gmail.com>
2020-10-30 22:47:55 +0700

    Добавляет собранный проект

--- src/js/catalog-list-toggle.js
+++ src/js/catalog-list-toggle.js
@@ -1,6 +1,8 @@
-const catalogMenu = document.querySelector(`.catalog-menu`);
-const catalogMenuRollupBtn = document.querySelector(`.page-header__catalog-rollup-btn`);
+(function () {
+  const catalogMenu = document.querySelector(`.catalog-menu`);
+  const catalogMenuRollupBtn = document.querySelector(`.page-header__catalog-rollup-btn`);
 
-catalogMenuRollupBtn.addEventListener(`click`, () => {
-  catalogMenu.classList.toggle(`visually-hidden`);
-});
+  catalogMenuRollupBtn.addEventListener(`click`, () => {
+    catalogMenu.classList.toggle(`visually-hidden`);
+  });
+})();
